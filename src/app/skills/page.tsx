e1286e2b4d92bcb5dcc8844378d422180a80579a
HongJinSuk <realtone98@penta.co.kr>
2024-12-26 18:03:30 +0900

    feat: minor

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,9 @@
+import { DataTableDemo } from '@/components/ui/data-table';
+
+export default function Page() {
+  return (
+    <div className="w-full h-full">
+      <DataTableDemo />
+    </div>
+  );
+}
